51c720fe0632774a3781ba90cbe8524ba90f8882
ycazeau <ycazeau23@gmail.com>
2022-10-26 21:54:30 -0400

    Designing task list page

--- base/views.py
+++ base/views.py
@@ -1,8 +1,10 @@
-from http.client import HTTPResponse
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
 # Create your views here.
 
-def taskList(request):
-    return HttpResponse('To Do List App')
+class TaskList(ListView):
+    model = Task
+    context_object_name= 'tasks'
