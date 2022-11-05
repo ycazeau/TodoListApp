5719169347a603414b1cadee6eaee2b8b6bb5f56
ycazeau <ycazeau23@gmail.com>
2022-11-05 00:48:42 -0400

    Styling the pages

--- base/views.py
+++ base/views.py
@@ -43,7 +43,7 @@ class RegisterPage(FormView):
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
             return redirect('tasks')
-        return super(RegisterPage, self).get(**args, **kwargs)
+        return super(RegisterPage, self).get(*args, **kwargs)
         
 # All views
 # All taks view
